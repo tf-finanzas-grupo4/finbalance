95cd081628adf98c2af81ca101f96e1f780f626d
Tato <italoluna23@gmail.com>
2025-07-10 12:36:11 -0500

    fix: update DEBUG setting to use environment variable for better security

--- finbalance/finbalance/settings.py
+++ finbalance/finbalance/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m!#ls%uglwa@95o@fq*_lki3&zu7*w%b=f@=)*+m=v^-v)gi9n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ["finbalance.devcorp.pe", "localhost", "127.0.0.1"]
 
